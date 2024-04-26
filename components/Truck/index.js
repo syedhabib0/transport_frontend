50ae64e199e2edddf8eba2ffeba17d1af16da250
Sameer Ali <alisameer52718@gmail.com>
2024-04-26 20:09:17 +0500

    Truck IMage changed

--- components/Truck/index.js
+++ components/Truck/index.js
@@ -1,7 +1,7 @@
 import Image from "next/image"
 const Truck = () => {
   return (
-    <Image src={"/assets/icon/truck.svg"}  width={36} height={36} alt="truck-icon"/>
+    <Image src={"/assets/icon/truck.png"}  width={36} height={36} alt="truck-icon"/>
   )
 }
 
